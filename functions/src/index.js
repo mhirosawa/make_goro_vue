3df246f5b4aef164925e10538a1a712d07adc884
mhirosawa@gmail.com <cm7ad5xdm>
2021-08-22 15:49:36 +0900

    コード整理

--- functions/src/index.js
+++ functions/src/index.js
@@ -5,32 +5,10 @@ import { find_goro, makeStatusObj } from './make_goro.js';
 import functions from "firebase-functions";
 
 
-exports.make_goro_org = async (req, res) => {
-
-	let statusObj = makeStatusObj();
-
-//	let str = await main( 'test' );
-//	res.send( str + "\n" + `Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
-
-	let keyword = req.query.keyword || req.body.keyword;
-	let limit = 20;
-	if ( req.query.limit || req.body.limit ) {
-		limit = parseInt( req.query.limit || req.body.limit );
-	}
-	await find_goro( statusObj, keyword, limit );
-	//console.log( "main : " + str );
-	res.set('Cache-Control', 'no-cache');
-	res.send( statusObj.PrintHtmlStr );
-};
-
 exports.make_goro = functions.https.onRequest( async (request, response) => {
 //  functions.logger.info("Hello logs!", {structuredData: true});
-//  response.send("Hello from Firebase!");
 
 	let statusObj = makeStatusObj();
-
-//	let str = await main( 'test' );
-//	res.send( str + "\n" + `Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
 
 	let keyword = request.query.keyword || request.body.keyword;
 	let limit = 20;
